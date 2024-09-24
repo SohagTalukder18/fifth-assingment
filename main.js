8a358a03012902693fc60a6e533c807614d0bf3b
SohagTalukder18 <sohag.cse180818@gmail.com>
2024-09-24 22:09:04 +0600

    added donate card section

--- main.js
+++ main.js
@@ -15,33 +15,72 @@ function validInput(id) {
 
 
 
+let successfullPopUp = document.getElementById("my_modal_4");
+let unSuccessfullPopUp = document.getElementById("my_modal_2");
 
-
-
-
-
-
-
+// Noakhali Flood
 
 document.getElementById('noakhali-donate').addEventListener('click', function (e) {
     e.preventDefault();
-    let modal = document.getElementById("my_modal_4");
-    let modal1 = document.getElementById("my_modal_2");
-    const totalBalance = document.getElementById('total-balance').innerText;
+ 
+    let totalBalance = document.getElementById('total-balance').innerText;
     const noakhaliTotalBalance = parseFloat(document.getElementById('noakhali-total-balance').innerText);
     const noakhaliDonateBalance = validInput('noakhali-donate-balance');
-    if(noakhaliDonateBalance === 0){
-        return modal1.showModal();
+    const newTotalBalance = totalBalance - noakhaliDonateBalance;
+    
+    if(noakhaliDonateBalance === 0 || newTotalBalance<0 ){
+        return unSuccessfullPopUp.showModal();
     }else{
         const noakhaliNewTotalBalance = noakhaliTotalBalance + noakhaliDonateBalance;
         document.getElementById('noakhali-total-balance').innerHTML = noakhaliNewTotalBalance;
-        const newTotalBalance = totalBalance - noakhaliDonateBalance;
         document.getElementById('total-balance').innerHTML = newTotalBalance;
-         modal.showModal();
+
+        successfullPopUp.showModal();
     }
+});
+// Feni Flood
+document.getElementById('feni-donate').addEventListener('click', function (e) {
+    e.preventDefault();
+ 
+    let totalBalance = document.getElementById('total-balance').innerText;
+   
+    const feniTotalBalance = parseFloat(document.getElementById('feni-total-balance').innerText);
 
+    const feniDonateBalance = validInput('feni-donate-balance');
+    const newTotalBalance = totalBalance - feniDonateBalance;
+    
+    if(feniDonateBalance === 0 || newTotalBalance<0 ){
+        return unSuccessfullPopUp.showModal();
+    }else{
+        const feniNewTotalBalance = feniTotalBalance + feniDonateBalance;
+        document.getElementById('feni-total-balance').innerHTML = feniNewTotalBalance;
+        document.getElementById('total-balance').innerHTML = newTotalBalance;
 
-})
+        successfullPopUp.showModal();
+    }
+});
+// Quota Movemen
+document.getElementById('quota-donate').addEventListener('click', function (e) {
+    e.preventDefault();
+ 
+    let totalBalance = document.getElementById('total-balance').innerText;
+   
+    const quotaTotalBalance = parseFloat(document.getElementById('quota-total-balance').innerText);
+
+    const quotaDonateBalance = validInput('quota-donate-balance');
+    const newTotalBalance = totalBalance - quotaDonateBalance;
+    
+    if(quotaDonateBalance === 0 || newTotalBalance<0 ){
+        return unSuccessfullPopUp.showModal();
+    }else{
+        const quotaNewTotalBalance = quotaTotalBalance + quotaDonateBalance;
+        document.getElementById('quota-total-balance').innerHTML = quotaNewTotalBalance;
+        document.getElementById('total-balance').innerHTML = newTotalBalance;
+
+        successfullPopUp.showModal();
+    }
+});
+
 
 
 
